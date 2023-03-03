8a677813f431790d31cf1ea82780742a169cb0a7
Ratchapon Prangthong <ppete2547@gmail.com>
2023-03-03 15:04:59 +0700

    lab23_fish

--- average.cpp
+++ average.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include <cstdlib>
+#include<string>
+using namespace std;
+
+int main(int argc,char* argv[]){
+ float sum = 0,avg;
+if(argc > 1){
+
+    for(int i = 0;i < argc;i++){
+        sum = sum + atof(argv[i]);
+    }
+        avg = sum/(argc-1);
+        cout << "---------------------------------" << endl;
+        cout << "Average of " << argc-1 << " numbers = " << avg <<endl;
+        cout << "---------------------------------";
+}
+else {
+    cout << "Please input numbers to find average.";
+}
+
+}
+
 
